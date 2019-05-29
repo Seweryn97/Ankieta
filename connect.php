9458fa4dfb332858cc2571cd5b1244600e9e77eb
Seweryn <sewerynde@gmail.com>
2019-05-29 12:27:56 +0200

    Etap końcowy

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
-$host="localhost";
-$db_user="root";
-$db_password="";
+$host="ankieta.cnwyt25nn1pc.eu-west-1.rds.amazonaws.com";
+$db_user="szaman13";
+$db_password="Dupa1234";
 $db_name="ankieta";
 ?>
